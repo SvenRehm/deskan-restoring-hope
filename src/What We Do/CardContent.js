bc8a751a976a09c022dd084c886887ab3422fc70
SvenRehm <rehm21@web.de>
2022-06-30 22:28:37 +0200

    picture in what we do segment

--- src/What We Do/CardContent.js
+++ src/What We Do/CardContent.js
@@ -1,45 +1,45 @@
-// import img1 from "../Images/woman sitting in front of closed door.jpg"
-// import img2 from "../Images/Free Man.jpg"
-// import img3 from "../Images/Soldier Giving.jpg"
-// import img5 from "../Images/gift box.jpg"
-// import img6 from "../Images/"
-import img7 from "../Images/hannah.jpg"
+import img1 from "../Images/woman sitting in front of closed door.jpg"
+import img2 from "../Images/johnmoeses.jpg"
+import img3 from "../Images/genebrutty.jpg"
+import img4 from "../Images/DSC_0558.jpg"
+import img5 from "../Images/gift box.jpg"
+import img6 from "../Images/closet.jpg"
 
 export let whatWeDoContent = [
    {
       title: "Veterans Services",
       text: "Veterans have disproportionate rates of mental illness, particularly posttraumatic stress disorder (PTSD).",
-      img: img7,
+      img: img1,
       link: "/deskan-restoring-hope/veterans",
    },
    {
       title: "Helping The Homeless",
       text: "Homelessness is not a choice.... Help IS! Homelessness is not unique to in town Atlanta communities.",
-      img: img7,
+      img: img2,
       link: "/deskan-restoring-hope/homeless",
    },
    {
       title: "National Poverty & Homelessness",
       text: "Nationwide, two factors have contributed to the increase in homelessness over the past several years.",
-      img: img7,
+      img: img3,
       link: "/deskan-restoring-hope/achgiving",
    },
    {
       title: "Medical Mission & Missionary Work",
       text: "This program contributes to over 2,000 families, personal hygiene products, clothes, food, household goods, & more.",
-      img: img7,
+      img: img4,
       link: "/deskan-restoring-hope/mmwork",
    },
    {
       title: "Annual Community Holiday Giving",
       text: "The Community Holiday Giving Program distributes to over 2,000 families",
-      img: img7,
+      img: img5,
       link: "/deskan-restoring-hope/achgiving",
    },
    {
       title: "Clothing Closet",
       text: "Deskan has provided clothing to local organizations that share in the cause of “Second Chance for Individuals”.",
-      img: img7,
+      img: img6,
       link: "/deskan-restoring-hope/food-pantry",
    },
 ]
